5efea6be13cc32e633e129b012ddc1f26f1cd7fc
khamal45 <mahendra_21510020@stimata.ac.id>
2023-12-21 16:49:30 +0700

    [FEATURE] API TEMPERATE

--- CloudComputing/CloudComputing/app/Http/Controllers/AuthController.php
+++ CloudComputing/CloudComputing/app/Http/Controllers/AuthController.php
@@ -11,61 +11,68 @@ class AuthController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login','register']]);
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
     }
 
-    public function login(Request $request){
-        $validator = Validator::make($request->all(),[
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
             'email' => 'required|email',
             'password' => 'required|string|min:8'
         ]);
-        if($validator->fails()){
-            return response()->json($validator->errors(),422);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
         }
-        if(!$token = Auth::attempt($validator->validated())){
-            return response()->json(['error' => 'Unauthorized'],401);
+        if (!$token = Auth::attempt($validator->validated())) {
+            return response()->json(['error' => 'Unauthorized'], 401);
         }
         return $this->createNewToken($token);
     }
 
-    public function register(Request $request){
-        $validator = Validator::make($request->all(),[
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
             'name' => 'required|string|between:2,100',
             'email' => 'required|email|unique:users',
             'password' => 'required|string|confirmed|min:8',
             'password_confirmation' => 'required|string|min:8'
         ]);
-        if($validator->fails()){
-            return response()->json($validator->errors()->toJson(),400);
+        if ($validator->fails()) {
+            return response()->json($validator->errors()->toJson(), 400);
         }
         $user = User::create(array_merge(
             $validator->validated(),
             ['password' => bcrypt($request->password)]
         ));
+
         return response()->json([
             'message' => 'User successfully registered',
             'user' => $user
-        ],201);
+        ], 201);
     }
 
-    public function logout(){
+    public function logout()
+    {
         Auth::logout();
         return response()->json(['message' => 'User successfully signed out']);
     }
 
-    public function refresh(){
+    public function refresh()
+    {
         return $this->createNewToken(Auth::refresh());
     }
 
-    public function userProfile(){
+    public function userProfile()
+    {
         return response()->json(Auth::user());
     }
 
-    protected function createNewToken($token){
+    protected function createNewToken($token)
+    {
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => Auth::factory()->getTTL()*60,
+            'expires_in' => Auth::factory()->getTTL() * 60,
             'user' => Auth::user()
         ]);
     }
